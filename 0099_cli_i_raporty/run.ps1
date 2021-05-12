a590f2395341c3125c6c3427f279d9171c8c93ce
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-05-12 16:31:40 +0200

    exampel app done

--- 0099_cli_i_raporty/run.ps1
+++ 0099_cli_i_raporty/run.ps1
@@ -1,4 +1,4 @@
 $path="C:\Users\gstarczewski\tools\apache-jmeter-5.2.1\bin"
 $testName="test-0099.jmx"
-$JMeterArgs="-f -l results/results.csv -e -o results/html"
+$JMeterArgs="-j results/jmeter.log -f -l results/results.csv -e -o results/html"
 Invoke-Expression "$path\jmeter.bat -n -t $testName $JMeterArgs"
