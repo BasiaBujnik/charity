a8ef14f855381faf865043e41840cf30769d25a8
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-07-20 11:05:36 +0200

    changed order

--- 103_elementy_skryptowalne_JSR223/103_Variables_Properties_UDV/UserDefinedVariables.ps1
+++ 103_elementy_skryptowalne_JSR223/103_Variables_Properties_UDV/UserDefinedVariables.ps1
@@ -1,6 +1,11 @@
 Remove-Item "$PSScriptRoot/results" -Recurse
+Remove-Item "$PSScriptRoot/report-output" -Recurse
+Remove-Item "$PSScriptRoot/logs" -Recurse
+
+New-Item -Path "$PSScriptRoot" -Name "html" -ItemType "directory" -Force
+New-Item -Path "$PSScriptRoot" -Name "results" -ItemType "directory" -Force
+New-Item -Path "$PSScriptRoot" -Name "logs" -ItemType "directory" -Force
+
 $path="C:\Users\gstarczewski\tools\apache-jmeter-5.4.1\bin"
 $testName="UserDefinedVariables.jmx"
-$JMeterArgs="-Judv=3333 -j $PSScriptRoot/results/jmeter.log -e -o $PSScriptRoot/results -l $PSScriptRoot/results/results.csv"
-Write-Host "$path\jmeter.bat -n -t $testName $JMeterArgs"
-Invoke-Expression "$path\jmeter.bat -n -t $testName $JMeterArgs"
+Invoke-Expression "$path\jmeter.bat -LDEBUG -f -n -t $testName -j logs\jmeter.log -l results\results.csv -Judv=3333"
