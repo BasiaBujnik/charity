82d8f3bbacd1697a88e6d77fe089087e64f859e7
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-07-20 11:11:48 +0200

    changed order

--- 103_elementy_skryptowalne_JSR223/103_Variables_Properties_UDV/UserDefinedVariables.ps1
+++ 103_elementy_skryptowalne_JSR223/103_Variables_Properties_UDV/UserDefinedVariables.ps1
@@ -1,6 +1,6 @@
-Remove-Item "$PSScriptRoot/results" -Recurse
-Remove-Item "$PSScriptRoot/report-output" -Recurse
-Remove-Item "$PSScriptRoot/logs" -Recurse
+Remove-Item "$PSScriptRoot/results" -Recurse -erroraction 'silentlycontinue'
+Remove-Item "$PSScriptRoot/report-output" -Recurse -erroraction 'silentlycontinue'
+Remove-Item "$PSScriptRoot/logs" -Recurse -erroraction 'silentlycontinue'
 
 New-Item -Path "$PSScriptRoot" -Name "html" -ItemType "directory" -Force
 New-Item -Path "$PSScriptRoot" -Name "results" -ItemType "directory" -Force
@@ -8,4 +8,5 @@ New-Item -Path "$PSScriptRoot" -Name "logs" -ItemType "directory" -Force
 
 $path="C:\Users\gstarczewski\tools\apache-jmeter-5.4.1\bin"
 $testName="UserDefinedVariables.jmx"
-Invoke-Expression "$path\jmeter.bat -LDEBUG -f -n -t $testName -j logs\jmeter.log -l results\results.csv -Judv=3333"
+Invoke-Expression "$path\jmeter.bat -LDEBUG -f -n -t $testName -j logs\jmeter.log -l results\results.csv -Judv=3333" #udv prop set in command
+#Invoke-Expression "$path\jmeter.bat -LDEBUG -f -n -t $testName -j logs\jmeter.log -l results\results.csv" #udv prop read from user.properties
