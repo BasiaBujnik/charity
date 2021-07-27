35ba474757a9046221389de593108e121838d10a
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-07-27 15:39:40 +0200

    changed order

--- 103_elementy_skryptowalne_JSR223/Variables/test-variables-begin.ps1
+++ 103_elementy_skryptowalne_JSR223/Variables/test-variables-begin.ps1
@@ -7,6 +7,6 @@ New-Item -Path "$PSScriptRoot" -Name "results" -ItemType "directory" -Force
 New-Item -Path "$PSScriptRoot" -Name "logs" -ItemType "directory" -Force
 
 $path="C:\Users\gstarczewski\tools\apache-jmeter-5.4.1\bin"
-$testName="UserDefinedVariables.jmx"
+$testName="test-variables-demo_result.jmx"
 Invoke-Expression "$path\jmeter.bat -LDEBUG -f -n -t $testName -j logs\jmeter.log -l results\results.csv -Judv=3333" #udv prop set in command
 #Invoke-Expression "$path\jmeter.bat -LDEBUG -f -n -t $testName -j logs\jmeter.log -l results\results.csv" #udv prop read from user.properties
--- 103_elementy_skryptowalne_JSR223/Variables/test-variables-todo_result.ps1
+++ 103_elementy_skryptowalne_JSR223/Variables/test-variables-todo_result.ps1
@@ -1,4 +1,4 @@
 $path="C:\Users\gstarczewski\tools\apache-jmeter-5.2.1\bin"
-$testName="odpowiedz.jmx"
+$testName="test-variables-todo_result.jmx"
 $JMeterArgs="-Jthreads=1 -Jloops=2 -JrampUp=1 -j results/jmeter.log -l results/results.csv"
 Invoke-Expression "$path\jmeter.bat -f -n -t $testName ${JMeterArgs}"
