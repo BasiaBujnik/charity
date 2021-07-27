15f1bb3656f5ba6ba2e5f58b13c6e870a1547a76
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-07-27 15:38:49 +0200

    changed order

--- 103_elementy_skryptowalne_JSR223/Variables/test-variables-todo_result.ps1
+++ 103_elementy_skryptowalne_JSR223/Variables/test-variables-todo_result.ps1
@@ -1,4 +1,4 @@
 $path="C:\Users\gstarczewski\tools\apache-jmeter-5.2.1\bin"
 $testName="odpowiedz.jmx"
-$JMeterArgs="-Jthreads=1 -Jloops=2 -JrampUp=1 -j results/jmeter.log -l results/results.csv -e -o results/html"
+$JMeterArgs="-Jthreads=1 -Jloops=2 -JrampUp=1 -j results/jmeter.log -l results/results.csv"
 Invoke-Expression "$path\jmeter.bat -f -n -t $testName ${JMeterArgs}"
